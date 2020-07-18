dea5a09b9563a3dc470ce6ed3814511c12c4b7b7
Pepa <eles.peter.12@gmail.com>
2020-07-18 21:55:02 +0200

    Add project files.

---
+++ Progmasters.Mordor/Models/Orc.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Progmasters.Mordor.Models
+{
+    public class Orc
+    {
+    }
+}
